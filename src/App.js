25cd0ecf1dbcbeed22634d0ef606de4ed5aa1115
Alfiado Ngana <alfiadoprojectos@gmail.com>
2020-01-14 16:48:12 +0200

    criando versao estatica do mapbox

--- src/App.js
+++ src/App.js
@@ -1,3 +1,75 @@
+import dotenv from 'dotenv';
 import React, { Component } from 'react';
 import MapGl, { Marker } from 'react-map-gl';
 
+dotenv.config();
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      viewport: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        latitude: -23.5439948,
+        longitude: -46.6065452,
+        zoom: 14,
+      },
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this._resize);
+    this._resize();
+  }
+
+  _resize = () => {
+    this.setState({
+      viewport: {
+        ...this.state.viewport,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      },
+    });
+  };
+
+  componentWillMount() {
+    window.removeEventListener('resize', this._resize);
+  }
+
+  handleMapClick(e) {
+    const [longitude, latitude] = e.lngLat;
+
+    alert(`Latitude: ${latitude} \nLongitude: ${longitude}`);
+  }
+
+  render() {
+    return (
+      <MapGl
+        {...this.state.viewport}
+        onClick={this.handleMapClick}
+        mapStyle="mapbox://styles/mapbox/basic-v9"
+        mapboxApiAccessToken={
+          'pk.eyJ1IjoiYWxmaWFkbyIsImEiOiJjazVkdHk5bnIwZHp2M21vM2I1cnRzN2liIn0.MUajuSREgajmY8msvP0izg'
+        }
+        onViewportChange={viewport => this.setState({ viewport })}
+      >
+        <Marker
+          latitude={-23.5439948}
+          longitude={-46.6065452}
+          onClick={this.handleMapClick}
+          captureClick={true}
+        >
+          <img
+            alt="map"
+            style={{
+              borderRadius: 100,
+              width: 48,
+              height: 48,
+            }}
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+          />
+        </Marker>
+      </MapGl>
+    );
+  }
+}
